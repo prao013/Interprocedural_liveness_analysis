2df34c82472bc866884e08193de954b8d92068d6
P Suhas Rao <prao013@ucr.edu>
2023-03-11 14:56:24 -0800

    Update ValueNumbering.cpp

--- Pass/Transforms/ValueNumbering/ValueNumbering.cpp
+++ Pass/Transforms/ValueNumbering/ValueNumbering.cpp
@@ -25,38 +25,144 @@ using namespace llvm;
 
 namespace {
 	set<string> killer(Function &F){
-	set<string> kill;	
-		for (auto& basic_block : F)
+	
+	string func_name = "minitest";
+	int count=0;
+	map<string, set<string> > PREDBBMAP;
+	
+	map<string, set<string> > LIVEOUT;
+	map<string, set<string> > UEVAR;
+	map<string, set<string> > VARKILL;
+        for (auto& basic_block : F)
         {	
-	    	
-            for (auto& inst : basic_block)
-            {
-		if(inst.getOpcode() == Instruction::Store){
- 			string operand2=inst.getOperand(1)->getName().str();
-			kill.insert(operand2);
-		}
-	    }
-				
-	}
-		return kill;
-	}
-	set<string> availabler(set<string> kill,Function &F)
-	{
-		set<string> live;	
+	if (F.getName() != func_name) {continue;}
 	for (auto& basic_block : F)
-        {	
-	 	
-            for (auto& inst : basic_block)
-            {
-		if(inst.getOpcode() == Instruction::Load){
- 			string operand1=inst.getOperand(0)->getName().str();
- 			auto it=kill.find(operand1);
-		 if ( it == kill.end() )
-			{live.insert(operand1);}
- 		} 
-	    }
+ 	{count++;}
+	string bbs[count];
+	for (auto& basic_block : F)
+ 	{count--;
+	bbs[count]=basic_block.getName().str();
 	}
-		return live;
+	for (auto& basic_block : F)
+ 	{count++;}
+	for (auto& basic_block : F){
+	for (auto it = pred_begin(&basic_block), et =pred_end(&basic_block); it != et; ++it){
+	BasicBlock* predecessor = *it;
+	PREDBBMAP[basic_block.getName().str()].insert(predecessor->getName().str());
+	}}
+ 	for (auto& basic_block : F)
+ 	{
+	for (auto& inst : basic_block)
+ 	{
+		if(inst.getOpcode() == Instruction::Store){
+		string operand2=inst.getOperand(1)->getName().str();
+		VARKILL[basic_block.getName().str()].insert(operand2);
+ 		}
+ 		if(inst.getOpcode() == Instruction::Load){
+ 			string operand1=inst.getOperand(0)->getName().str();
+	 		auto it=VARKILL[basic_block.getName().str()].find(operand1);
+ 			if ( it ==VARKILL[basic_block.getName().str()].end() )
+			{UEVAR[basic_block.getName().str()].insert(operand1);}
+ 		}
+            } // end for inst
+        } // end for basicblockwrite
+		
+	
+	for(int i=0;i<count;i++){
+		LIVEOUT[bbs[i]];
+		}
+	set<string>::iterator itrr;
+	set<string> HOLDER;
+	int i=2;
+	while(i>0){
+	for(int i=0;i<count;i++){
+ 	for (itrr = PREDBBMAP[bbs[i]].begin();itrr != PREDBBMAP[bbs[i]].end(); itrr++)
+ 	{
+	set_difference(LIVEOUT[bbs[i]].begin(),LIVEOUT[bbs[i]].end(), VARKILL[bbs[i]].begin(),VARKILL[bbs[i]].end(),std::inserter(HOLDER, HOLDER.end()));
+	HOLDER.insert(UEVAR[bbs[i]].begin(),UEVAR[bbs[i]].end());
+	LIVEOUT[*itrr].insert(HOLDER.begin(),HOLDER.end());
+	HOLDER.clear();
+	}}
+	i--;
+	}
+	}	
+ 	} // end for basicblockread
+	}
+	return VARKILL['entry'];
+	}
+	
+
+
+
+
+
+
+	set<string> availabler(Function &F)
+	{
+	
+	string func_name = "minitest";
+	int count=0;
+	map<string, set<string> > PREDBBMAP;
+	
+	map<string, set<string> > LIVEOUT;
+	map<string, set<string> > UEVAR;
+	map<string, set<string> > VARKILL;
+        for (auto& basic_block : F)
+        {	
+	if (F.getName() != func_name) {continue;}
+	for (auto& basic_block : F)
+ 	{count++;}
+	string bbs[count];
+	for (auto& basic_block : F)
+ 	{count--;
+	bbs[count]=basic_block.getName().str();
+	}
+	for (auto& basic_block : F)
+ 	{count++;}
+	for (auto& basic_block : F){
+	for (auto it = pred_begin(&basic_block), et =pred_end(&basic_block); it != et; ++it){
+	BasicBlock* predecessor = *it;
+	PREDBBMAP[basic_block.getName().str()].insert(predecessor->getName().str());
+	}}
+ 	for (auto& basic_block : F)
+ 	{
+	for (auto& inst : basic_block)
+ 	{
+		if(inst.getOpcode() == Instruction::Store){
+		string operand2=inst.getOperand(1)->getName().str();
+		VARKILL[basic_block.getName().str()].insert(operand2);
+ 		}
+ 		if(inst.getOpcode() == Instruction::Load){
+ 			string operand1=inst.getOperand(0)->getName().str();
+	 		auto it=VARKILL[basic_block.getName().str()].find(operand1);
+ 			if ( it ==VARKILL[basic_block.getName().str()].end() )
+			{UEVAR[basic_block.getName().str()].insert(operand1);}
+ 		}
+            } // end for inst
+        } // end for basicblockwrite
+		
+	
+	for(int i=0;i<count;i++){
+		LIVEOUT[bbs[i]];
+		}
+	set<string>::iterator itrr;
+	set<string> HOLDER;
+	int i=2;
+	while(i>0){
+	for(int i=0;i<count;i++){
+ 	for (itrr = PREDBBMAP[bbs[i]].begin();itrr != PREDBBMAP[bbs[i]].end(); itrr++)
+ 	{
+	set_difference(LIVEOUT[bbs[i]].begin(),LIVEOUT[bbs[i]].end(), VARKILL[bbs[i]].begin(),VARKILL[bbs[i]].end(),std::inserter(HOLDER, HOLDER.end()));
+	HOLDER.insert(UEVAR[bbs[i]].begin(),UEVAR[bbs[i]].end());
+	LIVEOUT[*itrr].insert(HOLDER.begin(),HOLDER.end());
+	HOLDER.clear();
+	}}
+	i--;
+	}
+	}	
+ 	} // end for basicblockread
+	}
+	return UEVAR['entry'];	
 	}
 	
 	
@@ -105,7 +211,7 @@ void visitor(Function &F){
 			set<string> kill;
 			set<string> available;
 			kill=killer(H);
-			available=availabler(kill,H);
+			available=availabler(H);
 			set<string>::iterator itr;
   			for (itr = kill.begin();itr != kill.end(); itr++)
 			{VARKILL[basic_block.getName().str()].insert(*itr);}
