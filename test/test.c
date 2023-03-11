abe05e46226d0de3df4e232c319ce0726b3855f8
P Suhas Rao <prao013@ucr.edu>
2023-03-10 16:20:52 -0800

    Update test.c

--- test/test.c
+++ test/test.c
@@ -1,6 +1,6 @@
-int minitest(int a){
-  a=a+1;
-  return a;
+int minitest(int b){
+  b=b+1;
+  return b;
 }
 
 
@@ -10,10 +10,12 @@ int test(int a, int b)
   int d = b + c;
   int e = a + b;
   int f = b + e;
+  
   if(a>0){
   minitest(a);
   } 
-  c=a;
+  
+  a=1;
   return f;
   
 }
