32c2ab5af7386c830201038a341ef2e8079fdfed
P Suhas Rao <prao013@ucr.edu>
2023-03-12 14:42:33 -0700

    Update ValueNumbering.cpp

--- Pass/Transforms/ValueNumbering/ValueNumbering.cpp
+++ Pass/Transforms/ValueNumbering/ValueNumbering.cpp
@@ -79,7 +79,7 @@ namespace {
 	}
 	
 	}	
-	for (itr1 = VARKILL["enrty"].begin(); itr1 != VARKILL["entry"].end(); itr1++)
+	for (itr1 = VARKILL["entry"].begin(); itr1 != VARKILL["entry"].end(); itr1++)
  		{
 		errs() <<" "<< *itr1;
 		}	
