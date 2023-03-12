d834a6cdf39a778b15cc47e6918cf7c7df7f87a4
P Suhas Rao <prao013@ucr.edu>
2023-03-12 15:04:20 -0700

    Update ValueNumbering.cpp

--- Pass/Transforms/ValueNumbering/ValueNumbering.cpp
+++ Pass/Transforms/ValueNumbering/ValueNumbering.cpp
@@ -83,7 +83,7 @@ namespace {
 	//}
 	
 	}	
-	for (itr1 = VARKILL["if.then"].begin(); itr1 != VARKILL["if.then"].end(); itr1++)
+	for (itr1 = VARKILL["if.else"].begin(); itr1 != VARKILL["if.else"].end(); itr1++)
  		{
 		errs() <<" "<< *itr1;
 		}	
